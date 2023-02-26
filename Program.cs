4921463e2e48b1b8d9ed0bdfe7b105be720897fa
rderoncio <rafael.deroncio@hotmail.com>
2023-02-26 17:42:17 -0300

    add configuração do projeto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// ConfigureServices
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-app.MapGet("/", () => "Hello World!");
-
+// Configure
+WebApplication app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapDefaultControllerRoute();
 app.Run();
